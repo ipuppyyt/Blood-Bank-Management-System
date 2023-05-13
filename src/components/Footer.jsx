309cefc15d729fbc4ff66b578964888e3904108a
iPuppyYT <dhanush6102002@gmail.com>
2023-05-13 17:30:39 +0530

    Commented off footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ import './css/footer.css';
 
 const Footer = () => {
   return (
-    <div className='footer'>
+    <div class='footer'>
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" style={{backgroundColor:"#000"}}>
+      <AppBar style={{backgroundColor:"#000"}}>
         <Toolbar>
           <Typography variant="p" style={{fontWeight:"bold"}} component="div" sx={{ flexGrow: 1 }}>
             &copy; {(new Date().getFullYear())} Blood Bank Management System. All Rights Reserved.

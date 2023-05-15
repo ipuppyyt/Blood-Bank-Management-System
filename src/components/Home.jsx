9a65a65bf1111cd923f2ee01775894c84ee9bec8
iPuppyYT <dhanush6102002@gmail.com>
2023-05-15 18:06:55 +0530

    added home bg

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import Navatar from './Navatar'
+import './css/homebg.css'
+import './css/home.css'
 
 const Home = () => {
 
 
   return (
-  <div>
+    <section>
       <Navatar />
-      
-{/* https://static.vecteezy.com/system/resources/previews/001/978/288/original/volunteer-collecting-blood-donation-free-vector.jpg */}
-       </div>
+  <div class='air air1'></div>
+  <div class='air air2'></div>
+  <div class='air air3'></div>
+  <div class='air air4'></div>
+</section>
        )
       }
 export default Home;
+
+// {/* https://static.vecteezy.com/system/resources/previews/001/978/288/original/volunteer-collecting-blood-donation-free-vector.jpg */}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ return (
     <div>
       <div class="navbar">
         <div class="navlogo">
-          <Link to="/" >
+          <Link to="/" style={{color:"white"}}>
             <img class="navlogo-img" src="https://th.bing.com/th/id/OIP.B-z7OG5fX6O6CQ7h4ITUQAHaHZ?pid=ImgDet&rs=1" alt="Navlogo"/>
             <h6 class="navtext">BBMS</h6>
           </Link>

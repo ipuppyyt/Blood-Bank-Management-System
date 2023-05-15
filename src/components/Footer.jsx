265d7086cdc55a3725895c0f8b531f8705055e62
iPuppyYT <dhanush6102002@gmail.com>
2023-05-15 16:52:21 +0530

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,35 @@
 import * as React from 'react';
 import './css/footer.css';
-import { Box, Container, Typography } from '@mui/material';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 const Footer = () => {
   return (
     <div class='footer'>
-          <Box component="footer" sx={{ py: 3, px: 2, mt: 'auto',
-          backgroundColor: (theme) =>
-            theme.palette.mode === 'light'
-              ? theme.palette.grey[200]
-              : theme.palette.grey[800],
-        }}>
-        <Container maxWidth="sm">
-          <Typography variant="p">
-          &copy; {(new Date().getFullYear())} Blood Bank Management System. All Rights Reserved.
-          </Typography>
-        </Container>
-      </Box>
+         <div class="footer-icons">
+          <div class="footer-text">© {(new Date().getFullYear())} Blood Bank Management System.</div>
+            {/* YouTube Icon */}
+            <div class="icon-bg yt">
+              <a href='https://www.youtube.com/' title='Youtube' target="_blank" rel='noreferrer'><YouTubeIcon style={{color:"white"}} /></a>
+            </div>&nbsp;
+
+            {/* Facebook Icon */}
+            <div class="icon-bg fb">
+              <a href='https://www.facebook.com/' title='Facebook' target="_blank" rel='noreferrer'><FacebookIcon style={{color:"white"}} /></a>
+            </div>&nbsp;
+
+            {/* Twitter Icon */}
+            <div class="icon-bg tw">
+              <a href='https://twitter.com/' title='Twitter' target="_blank" rel='noreferrer'><TwitterIcon style={{color:"white"}} /></a>
+            </div>&nbsp;
+
+            {/* Instagram Icon */}
+            <div class="icon-bg ig">
+              <a href='https://www.instagram.com/' title='Instagram' target="_blank" rel='noreferrer'><InstagramIcon style={{color:"white"}} /></a>
+            </div>
+         </div>
     </div>
   );
 };

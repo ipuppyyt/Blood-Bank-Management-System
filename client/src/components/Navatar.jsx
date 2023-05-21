f7264ff44d7b853917ae6594d61d235d3b785305
iPuppyYT <dhanush6102002@gmail.com>
2023-05-21 10:28:09 +0530

    navbar update for req page

--- client/src/components/Navatar.jsx
+++ client/src/components/Navatar.jsx
@@ -204,6 +204,47 @@ const Navatar = () => {
       </div>);
       return retnav;
     }
+
+
+    //Admin Request NavIcon
+    else if (window.location.pathname === "/admin/requests") {
+      let retnav = (<div><div className="dash-btn-left"><Link to="/admin/dashboard"><Button style={{ backgroundColor: blue[500], fontWeight: "bolder" }} variant="contained"><DashboardIcon style={{ fontSize: "medium" }}/>&nbsp;Dashboard</Button></Link>
+      &nbsp;&nbsp;</div><div className="dash-avatarr">
+        <React.Fragment>
+          <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
+            <Tooltip title="Account">
+              <IconButton onClick={handleClick} size="small" sx={{ ml: 2 }}
+                aria-controls={open ? "account-menu" : undefined} aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}>
+                <Avatar sx={{ width: 40, height: 40 }}>M</Avatar>
+              </IconButton>
+            </Tooltip>
+          </Box>
+          <Menu anchorEl={anchorEl} id="account-menu" open={open} onClose={handleClose}
+            onClick={handleClose} PaperProps={{
+              elevation: 0, sx: {
+                overflow: "visible",
+                filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))", mt: 1.5, "& .MuiAvatar-root": {
+                  width: 32, height: 32, ml: -0.5, mr: 1,
+                }, "&:before": {
+                  content: '""', display: "block", position: "absolute",
+                  top: 0, right: 14, width: 10,
+                  height: 10, bgcolor: "background.paper", transform: "translateY(-50%) rotate(45deg)",
+                  zIndex: 0,
+                },
+              },
+            }}
+            transformOrigin={{ horizontal: "right", vertical: "top" }}
+            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}>
+            <MenuItem onClick={handleClose}>
+              <ListItemIcon><LogoutIcon fontSize="small" /></ListItemIcon>Logout
+            </MenuItem>
+          </Menu>
+        </React.Fragment>
+      </div>
+      </div>);
+      return retnav;
+    }
   };
 
   return (

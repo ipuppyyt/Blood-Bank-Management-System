fc043a3f181602841f02d86097dbc482e500d6eb
iPuppyYT <dhanush6102002@gmail.com>
2023-05-27 00:46:21 +0530

    Update login.js

--- server/uncontroller/login.js
+++ server/uncontroller/login.js
@@ -8,7 +8,7 @@ loginRouter.post('/', async (request, response) => {
   const { userEmail, userPassword } = request.body
     console.log(userEmail)
   try{
-  const user = await User.findOne({ adminEmail : adminEmail, userName : userName  })
+  const user = await User.findOne({ userEmail : userEmail, userName : userName  })
   console.log(`User : ${user}`)
   console.log(`Password : ${user.userPassword}`)
   const passwordCorrect = user === null

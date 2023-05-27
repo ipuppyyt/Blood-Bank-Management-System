43641ac5a57ae6fb26903b71dd1d079a16bc4dcf
iPuppyYT <dhanush6102002@gmail.com>
2023-05-27 10:54:31 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -117,7 +117,8 @@ var transporter = nodemailer.createTransport({
 
 app.post('/user/new/adminverification', async (req, res) => {
     const request = await new adminVerified(req.body);
-    const userEmail = await userData.findOne({ userEmail: req.body.userEmail });
+    const userEmail = await new pendingVerification(req.body.userEmail);
+    console.log(userEmail.userEmail);
     const category = req.body.category;
     request.save();
     var mailOptions = {
